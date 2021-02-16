870cbe25f2f010ba8cc6da2292eedd52c330cd41
PezeM <16854655+PezeM@users.noreply.github.com>
2021-02-16 19:21:39 +0100

    Moved components to own files

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,11 @@
 import React from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
-import { Button, DatePicker } from 'antd';
 import './App.global.css';
 import { BaseLayout } from './containers/baseLayout';
 import { ROUTES } from './constants/routes';
 import { Error404 } from './components/error404';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectConfig, updateKey } from './features/config/configSlice';
-
-const Hello = () => {
-  const dispatch = useDispatch();
-  const config = useSelector(selectConfig);
-
-  return (
-    <div>
-      <DatePicker />
-      <Button type='primary' style={{ marginLeft: 8 }}
-              onClick={() => dispatch(updateKey({ key: 'logLevel', value: 'elo' }))}>
-        Primary Button
-        {config.logLevel}
-      </Button>
-      <div>
-        co: {config.clientLog}
-      </div>
-    </div>
-  );
-};
-
-const Settings = () => {
-  return (
-    <div>
-      <h1>Settings</h1>
-    </div>
-  );
-};
+import { Settings } from './components/Settings';
+import { Hello } from './components/Hello';
 
 export default function App() {
   return (
