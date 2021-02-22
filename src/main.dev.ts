92ecc0ca5df2e7ab7197850107267bb29db777bb
PezeM <16854655+PezeM@users.noreply.github.com>
2021-02-22 17:13:57 +0100

    Prevent running multiple instances of app

--- src/main.dev.ts
+++ src/main.dev.ts
@@ -43,6 +43,21 @@ if (
   require('electron-debug')();
 }
 
+// Prevent running multiple instances of application
+const gotTheLock = app.requestSingleInstanceLock();
+
+if (!gotTheLock) {
+  app.quit();
+} else {
+  app.on('second-instance', () => {
+    // Someone tried to run a second instance, we should focus our window.
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore();
+      mainWindow.focus();
+    }
+  });
+}
+
 const installExtensions = async () => {
   const installer = require('electron-devtools-installer');
   const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
